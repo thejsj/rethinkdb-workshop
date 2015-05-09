3376719ccc5a98db24f4bc6f11217874817d7365
thejsj <jorge.silva@thejsj.com>
2015-05-09 10:30:05 -0600

    Add new connection on changfeed

--- server/index.js
+++ server/index.js
@@ -113,6 +113,8 @@ io.on('connection', function (socket) {
    * hint: the query will return a cursor, not an array
    * hint: the objects return by the cursor have a `new_val` and an `old_val` property
    *
+   * Instead of using `r.conn` as your connection, create a new connection with r.connect
+   *
    * callback instructions:
    * every time a change is pushed by the database, push that change to
    * the client by emitting a socket event:
@@ -122,12 +124,15 @@ io.on('connection', function (socket) {
    * once you write this query, you'll be able to see new messages be displayed
    * as they are being added
    */
-  r.table('messages')
-    .changes().run(r.conn)
-    .then(function(cursor) {
-      cursor.each(function (err, row) {
-        socket.emit('message', row.new_val);
-      }, function () { });
+  r.connect(config.get('rethinkdb'))
+    .then(function (conn) {
+      r.table('messages')
+        .changes().run(conn)
+        .then(function(cursor) {
+          cursor.each(function (err, row) {
+            socket.emit('message', row.new_val);
+          }, function () { });
+        });
     });
 
   // Insert new messages
