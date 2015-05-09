69ab053f91f42064a9989ad0723a447cde449456
thejsj <jorge.silva@thejsj.com>
2015-05-08 23:30:20 -0600

    Add config

---
+++ config/default.js
@@ -0,0 +1,15 @@
+/*jshint node:true */
+'use strict';
+var config = {
+  'rethinkdb': {
+    'host': 'localhost',
+    'port': 28015,
+    'db': 'rethinkdb_workshop'
+  },
+  'ports' : {
+    'http' : 8000
+  },
+  'url': '127.0.0.1',
+};
+
+module.exports = config;
